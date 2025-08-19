b98202c9ae709304adfe7ff1acf543e089d56de7
Youssef-Ai1001 <ytaha8586@gmail.com>
2025-08-19 13:23:49 +0300

    adding more logic of project like uploading files

--- src/routes/base.py
+++ src/routes/base.py
@@ -1,15 +1,18 @@
-from fastapi import FastAPI, APIRouter  
+from fastapi import FastAPI, APIRouter ,Depends
 import os
+from helpers.config import get_settings ,Settings
 
 base_router = APIRouter(
-    prefix="/base",
-    tags=["base"]
+    prefix="/api/v1",
+    tags=["api_v1"],
 )
 
 @base_router.get("/")
-def welcome():
-    app_name = os.getenv("APP_NAME")
-    app_version = os.getenv("APP_VERSION")
+async def welcome(app_settings : Settings = Depends(get_settings)):
+
+    app_name = app_settings.APP_NAME
+    app_version = app_settings.APP_VERSION
+
     return {
         "app_name": app_name,
         "app_version": app_version,
