f1d45b18f9c6b84904faf9c074cb0cefe6fac231
Youssef-Ai1001 <ytaha8586@gmail.com>
2025-10-01 18:36:02 +0300

    Updating DB Schema

--- src/models/db_schemas/data_chunk.py
+++ src/models/db_schemas/data_chunk.py
@@ -1,13 +1,16 @@
-from pydantic import BaseModel,Field,validator
+from pydantic import BaseModel,Field,validator, ConfigDict
 from typing import Optional
 from bson.objectid import ObjectId
 
 class DataChunk(BaseModel):
-    _id:Optional[ObjectId]
+    
+    model_config = ConfigDict(
+            arbitrary_types_allowed=True,
+            populate_by_name=True
+        )      
+    
+    id : Optional[ObjectId] = Field(default=None, alias="_id")
     chunk_content:str = Field(..., min_length=1)
     chunk_metadata:dict
     chunk_order:int = Field(...,gt=0)
     chunk_project_id:ObjectId
-    
-    class config:
-        arbitrary_types_allowed = True
