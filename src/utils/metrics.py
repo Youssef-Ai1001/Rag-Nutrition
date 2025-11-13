0116a9572bb25da70a2e3b70d18b1033b66ed86d
Youssef-Ai1001 <ytaha8586@gmail.com>
2025-11-13 14:31:02 +0200

    Define Prometheus metrics

---
+++ src/utils/metrics.py
@@ -0,0 +1,55 @@
+from prometheus_client import Counter, Histogram, generate_latest, CONTENT_TYPE_LATEST
+from fastapi import FastAPI, Request, Response
+from starlette.middleware.base import BaseHTTPMiddleware
+import time
+
+
+# Define Prometheus metrics
+REQUEST_COUNT = Counter(
+    "http_requests_total",
+    "Total number of HTTP requests",
+    ["method", "endpoint", "http_status"],
+)
+
+REQUEST_LATENCY = Histogram(
+    "http_request_latency_seconds",
+    "Latency of HTTP requests in seconds",
+    ["method", "endpoint"],
+)
+
+
+class PrometheusMiddleware(BaseHTTPMiddleware):
+    async def dispatch(self, request: Request, call_next):
+        start_time = time.time()
+
+        # Process the request
+        response = await call_next(request)
+
+        # Record metrics after request is processed
+        duration = time.time() - start_time
+        endpoint = request.url.path
+
+        REQUEST_COUNT.labels(
+            method = request.method,
+            endpoint = endpoint,
+            status = response.status_code
+            ).inc()
+        
+        REQUEST_LATENCY.labels(
+            method = request.method,
+            endpoint = endpoint
+            ).observe(duration)
+        
+        return response
+
+def setup_metrics(app: FastAPI):
+    """
+    Setup Prometheus metrics middleware and endpoint
+    """
+    
+    # Add Prometheus middleware
+    app.add_middleware(PrometheusMiddleware)
+    
+    @app.get("/TrhBVe_m5gg2002_E5VVqS", include_in_schema=False)
+    def metrics():
+        return Response(generate_latest(), media_type=CONTENT_TYPE_LATEST)
